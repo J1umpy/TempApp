b78738e9d9f2ef89673e6894ffd3c9cd31786932
Karl Morris <karl.morris@temple.edu>
2026-09-01 13:14:15 -0400

    Cleaned code

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val myTemp = Temperature(Temperature.Unit.K)
+    val myTemp: Temperature = Temperature(Temperature.Unit.K)
 
     myTemp.setTemp(0.0)
 
-    println("The temperature is ${myTemp.getTemp()}")
+    println("The temperature is ${myTemp.getTemp(Temperature.Unit.K)}")
 }
